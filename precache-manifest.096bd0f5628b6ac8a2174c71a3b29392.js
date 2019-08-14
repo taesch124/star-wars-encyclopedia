e61ae48f89619e174885886e85627b52139ebfab
taesch124 <taesch124@gmail.com>
2019-08-13 20:22:37 -0400

    Updates

---
+++ precache-manifest.096bd0f5628b6ac8a2174c71a3b29392.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d858cba21ac755f54265d97d1c12e038",
+    "url": "/star-wars-encyclopedia/index.html"
+  },
+  {
+    "revision": "d9f3e998a687162460a0",
+    "url": "/star-wars-encyclopedia/static/css/main.7af1497d.chunk.css"
+  },
+  {
+    "revision": "c4039aa14dbf0d22a920",
+    "url": "/star-wars-encyclopedia/static/js/2.a8872f4d.chunk.js"
+  },
+  {
+    "revision": "d9f3e998a687162460a0",
+    "url": "/star-wars-encyclopedia/static/js/main.ad523cd9.chunk.js"
+  },
+  {
+    "revision": "4f828462c0b69fd57b4d",
+    "url": "/star-wars-encyclopedia/static/js/runtime~main.eb48660a.js"
+  },
+  {
+    "revision": "af7c07faaff0d16720c25db7990579e2",
+    "url": "/star-wars-encyclopedia/static/media/Starjedi.af7c07fa.ttf"
+  }
+]);
